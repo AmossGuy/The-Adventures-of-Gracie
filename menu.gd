8d7f823649ce2b3b52159ddd318be08dce21b330
AmossGuy <yellowilya@gmail.com>
2023-03-12 12:49:05 -0400

    add enemy test level

--- menu.gd
+++ menu.gd
@@ -26,9 +26,13 @@ func add_button(label: String, action: Callable = func(): pass) -> void:
 func setup_main_menu() -> void:
 	start_new_menu("MAIN MENU")
 	add_button("Level Select", setup_level_select)
-	add_button("Quit", func(): get_tree().quit())
+	add_button("Quit", get_tree().quit)
 
 func setup_level_select() -> void:
 	start_new_menu("LEVEL SELECT")
 	add_button("Back", setup_main_menu)
-	add_button("Test Level", func(): get_tree().change_scene_to_file("res://levels/test.tscn"))
+	add_button("Basic Test", load_level.bind("res://levels/test.tscn"))
+	add_button("Enemy Test", load_level.bind("res://levels/test_enemies.tscn"))
+
+func load_level(level: String) -> void:
+	get_tree().change_scene_to_file(level)
