55ada94b1ec9f3763bb0bd122b52cd1b0fdaa040
AmossGuy <yellowilya@gmail.com>
2023-03-26 10:28:23 -0400

    add pause menu & level_manager

--- menu.gd
+++ menu.gd
@@ -1,37 +1,43 @@
-extends Node2D
+extends BoxContainer
 
-@onready var menu_container: BoxContainer = %menu_container
+@export var pause_menu: bool = false
 
 func _ready() -> void:
 	setup_main_menu()
 
 func start_new_menu(title: String) -> void:
-	for node in menu_container.get_children():
+	for node in get_children():
 		node.queue_free()
 	
 	var header := Label.new()
 	header.text = title
-	menu_container.add_child(header)
+	add_child(header)
 	
-	add_spacer()
+	add_gap()
 
-func add_spacer() -> void:
+func add_gap() -> void:
 	var spacer := Control.new()
 	spacer.custom_minimum_size.y = 4
-	menu_container.add_child(spacer)
+	add_child(spacer)
 
 func add_button(label: String, action: Callable = func(): pass) -> Button:
 	var button := Button.new()
 	button.text = label
 	button.pressed.connect(action)
-	menu_container.add_child(button)
+	add_child(button)
 	return button
 
 func setup_main_menu() -> void:
-	start_new_menu("MAIN MENU")
-	add_button("Level Select", setup_level_select)
+	start_new_menu("PAUSED" if pause_menu else "MAIN MENU")
+	if pause_menu:
+		add_button("Unpause", unpause)
+	if not pause_menu:
+		add_button("Level Select", setup_level_select)
 	add_button("Settings", setup_settings_menu)
-	add_button("Quit", get_tree().quit)
+	if not pause_menu:
+		add_button("Quit", get_tree().quit)
+	else:
+		add_button("Return to Menu", load_level.bind("res://menu.tscn"))
 
 func setup_level_select() -> void:
 	start_new_menu("LEVEL SELECT")
@@ -40,6 +46,7 @@ func setup_level_select() -> void:
 	add_button("Enemy Test", load_level.bind("res://levels/test_enemies.tscn"))
 
 func load_level(level: String) -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file(level)
 
 func setup_settings_menu() -> void:
@@ -58,5 +65,9 @@ func toggle_fullscreen() -> void:
 func setup_control_menu() -> void:
 	start_new_menu("CONTROL SETTINGS")
 	add_button("Back", setup_settings_menu)
-	add_spacer()
-	menu_container.add_child(preload("res://misc/control_binder.tscn").instantiate())
+	add_gap()
+	add_child(preload("res://misc/control_binder.tscn").instantiate())
+
+func unpause() -> void:
+	get_tree().paused = false
+	get_parent().queue_free()
