25c7bd7617bff8dfbeeed3e4d5c60ebb48bf787b
AmossGuy <yellowilya@gmail.com>
2023-05-17 14:43:55 -0400

    don’t crash when there’s no current scene

--- checkpoint_hack_please_refactor.gd
+++ checkpoint_hack_please_refactor.gd
@@ -4,7 +4,9 @@ var scene: String = ""
 var player_start: NodePath = ""
 
 func _process(_delta: float) -> void:
-	var current_scene = get_tree().current_scene.scene_file_path
+	var tree_scene := get_tree().current_scene
+	var current_scene := tree_scene.scene_file_path if tree_scene != null else ""
+	
 	if current_scene != scene:
 		scene = current_scene
 		player_start = ""
