8b78d4dfbc1ba5dbb6191caf064e02301f0f0be5
AmossGuy <yellowilya@gmail.com>
2023-06-06 10:47:12 -0400

    split trampoline scene, add low bounce

--- objects/dog.gd
+++ objects/dog.gd
@@ -145,7 +145,7 @@ func _physics_process(delta: float) -> void:
 	for i in get_slide_collision_count():
 		var collision := get_slide_collision(i)
 		if collision.get_collider().is_in_group("trampoline"):
-			velocity.y = -300
+			velocity.y = -300 if Input.is_action_pressed("jump") else -150
 	
 	if state == State.HURT and is_on_floor():
 		state = State.GROUND
